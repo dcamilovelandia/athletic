570991c22a643de3394b272936fc5eb6b64b0e8e
camilo1498 <104171332+dcamilovelandia@users.noreply.github.com>
2022-06-22 15:41:58 -0500

    first commit

--- lib/src/presentation/pages/referrals/referrals_page.dart
+++ lib/src/presentation/pages/referrals/referrals_page.dart
@@ -1,10 +1,15 @@
+import 'package:athletic/src/core/imports/app_imports.dart';
 import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
 
 class ReferralsPage extends StatelessWidget {
   const ReferralsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GetBuilder<>;
+    return GetBuilder<ReferralsController>(
+      init: ReferralsController(),
+      builder: (_) => Scaffold(),
+    );
   }
 }
